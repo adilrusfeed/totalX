0fe283703882284cb0e6d77ab84ba5d34a30c2e5
adilrusfeed <adilrusfeed@gmail.com>
2024-10-17 16:33:21 +0530

    app

--- lib/view/screens/home_screens.dart
+++ lib/view/screens/home_screens.dart
@@ -14,22 +14,34 @@ class HomeScreen extends StatelessWidget {
     provider.getUsersAndSort("All");
     return Scaffold(
       appBar: AppBar(
-        title: Text("User Data"),centerTitle: true,
+        title: Text("User Data",
+        style: TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1.2),),centerTitle: true,
         backgroundColor: Colors.black,
+        elevation: 5,
+                shadowColor: Colors.grey[800],
+
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
                 Expanded(
                   child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
                     margin: const EdgeInsets.symmetric(vertical: 10),
                     decoration: BoxDecoration(
                       color: Colors.grey[200],
                       borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[300]!.withOpacity(0.5),
+                          blurRadius: 10,
+                          offset: Offset(0, 4)
+                        )
+                      ]
                     ),
                     child: TextField(
                       decoration: const InputDecoration(
@@ -43,6 +55,7 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(width: 8),
                 IconButton(
                   onPressed: () {
                     showDialog(
@@ -91,8 +104,11 @@ class HomeScreen extends StatelessWidget {
                           return Card(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15),
-                            ),
+                              
+                            ),elevation: 3,
+                            shadowColor: Colors.grey[500],
                             child: ListTile(
+                              contentPadding: EdgeInsets.all(12),
                               leading: CircleAvatar(
                                 radius: 30,
                                 backgroundImage: NetworkImage(
@@ -123,6 +139,10 @@ class HomeScreen extends StatelessWidget {
         onPressed: () {
           showModalBottomSheet(
             isScrollControlled: true,
+            backgroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+            ),
             context: context,
             builder: (context) {
               return Padding(
--- lib/view/widgets/add_dialog.dart
+++ lib/view/widgets/add_dialog.dart
@@ -30,118 +30,121 @@ class _AddUserWidgetState extends State<AddUserWidget> {
     return Form(
       key: formKey,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Consumer<UserController>(
-              builder: (context, value, child) => GestureDetector(
-                onTap: () => value.pickImageFromGallery(),
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: value.pickedImage != null
-                      ? FileImage(value.pickedImage!)
-                      : null,
-                  child: value.pickedImage == null
-                      ? const Icon(Icons.person)
-                      : null,
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'Add A New User',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              focusNode: _nameFocusNode,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Please enter Name";
-                }
-                return null;
-              },
-              controller: provider.nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-              ),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_ageFocusNode);
-              },
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              focusNode: _ageFocusNode,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Please enter Age";
-                }
-                return null;
-              },
-              controller: provider.ageController,
-              decoration: const InputDecoration(
-                labelText: 'Age',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).unfocus();
-              },
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 236, 235, 235),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 15),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    'Cancel',
-                    style: TextStyle(color: Colors.grey),
+        padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Consumer<UserController>(
+                builder: (context, value, child) => GestureDetector(
+                  onTap: () => value.pickImageFromGallery(),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: value.pickedImage != null
+                        ? FileImage(value.pickedImage!)
+                        : null,
+                    child: value.pickedImage == null
+                        ? const Icon(Icons.person)
+                        : null,
+                        backgroundColor: Colors.grey,
                   ),
                 ),
-                const SizedBox(
-                  width: 10,
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Add A New User',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,letterSpacing: 1.2),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                focusNode: _nameFocusNode,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter Name";
+                  }
+                  return null;
+                },
+                controller: provider.nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF007bff),
-                    elevation: 6,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_ageFocusNode);
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                focusNode: _ageFocusNode,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter Age";
+                  }
+                  return null;
+                },
+                controller: provider.ageController,
+                decoration: const InputDecoration(
+                  labelText: 'Age',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).unfocus();
+                },
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 236, 235, 235),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 15),
                     ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 40,
-                      vertical: 13,
-                    ),
-                  ),
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      addData(context);
+                    onPressed: () {
                       Navigator.pop(context);
-                    }
-                  },
-                  child: const Text(
-                    'Save',
-                    style: TextStyle(color: Colors.white),
+                    },
+                    child: const Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.grey),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF007bff),
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 13,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        addData(context);
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
